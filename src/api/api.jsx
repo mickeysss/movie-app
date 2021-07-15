7d0139f6fd7bd60cdc20f19b15bb388d72908015
Michael Sargsyan <60982064+mickeysss@users.noreply.github.com>
2021-07-15 12:00:33 +0300

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,27 +3,13 @@ const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
 })
 
-// export const searchAPI = {
-//     getMoviesByName(page, movie){
-//         return instance.get(`search/movie?api_key=320ef2b5438de647cfe72fb45ed0efdf&language=ru&query=${movie}&page=${page}`)
-//     },
-
-//     getHighRatedMovies(page){
-//         return instance.get(`movie/top_rated?api_key=320ef2b5438de647cfe72fb45ed0efdf&language=ru${page}`)
-//     },
-//     getMovieCredits(movieId){
-//         return instance.get(`/movie/${movieId}?api_key=320ef2b5438de647cfe72fb45ed0efdf&language=ru`)
-//     },
-// }
-
-
 export const searchAPI = {
     getMoviesByName( page, movie){
         return instance.get(`search/movie?api_key=320ef2b5438de647cfe72fb45ed0efdf&language=ru&query=${movie}&page=${page}`)
     },
 
     getRatedMovies(page){
-        return instance.get(`/movie/top_rated?api_key=320ef2b5438de647cfe72fb45ed0efdf&language=${page}`)
+        return instance.get(`/movie/top_rated?api_key=320ef2b5438de647cfe72fb45ed0efdf&language=ru&${page}`)
     },
     getMovieCredits(movieId){
         return instance.get(`/movie/${movieId}?api_key=320ef2b5438de647cfe72fb45ed0efdf&language=ru`)
